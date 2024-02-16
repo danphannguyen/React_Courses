d804daa62aee679945782113fb509bb15d7d989d
Dan PN <77532827+Derlvx@users.noreply.github.com>
2024-02-16 12:09:54 -0500

    React ep5

--- src/App.js
+++ src/App.js
@@ -1,30 +1,60 @@
 import './App.css';
 import { useState } from "react";
+import { Task } from "./Task";
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  const addCount = () => {
-    setCount(count + 1)
-  }
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
-  const removeCount = () => {
-    setCount(count - 1)
-  }
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
 
-  const resetCount = () => {
-    setCount(0)
-  }
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    };
+    setTodoList(task.taskName !== "" ? [...todoList, task] : todoList);
+  };
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+
+  const completeTask = (id) => {
+      setTodoList(todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: !task.completed };
+        }
+        return task;
+      }
+      ));
+  };
 
 
   return (
     <div className="App">
-      <button onClick={addCount}>Increase</button>
-      <button onClick={removeCount}>Decrease</button>
-      <button onClick={resetCount}>Reset</button>
-
-      {count}
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}> Add Task</button>
+      </div>
+      <div className="list">
+        {todoList.map((task) => {
+          return (
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              completed={task.completed}
+              deleteTask={deleteTask}
+              completeTask={completeTask}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
