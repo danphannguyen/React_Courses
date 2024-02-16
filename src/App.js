f8fb6e85b8dfa35d4a6a08a11939f1c4878fee99
Dan PN <77532827+Derlvx@users.noreply.github.com>
2024-02-16 16:32:08 -0500

    React ep7

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import "./App.css";
-import { useState } from "react";
-import { Text } from "./text";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+
 
 function App() {
-  const [showText, setShowText] = useState(false);
+
+  const [excuse, setExcuse] = useState("");
+
+  const generateExcuse = (category) => {
+    let url = "https://excuser-three.vercel.app/v1/excuse/" + category;
+    Axios.get(url).then((response) => {
+      setExcuse(response.data[0].excuse);
+    })
+  }
 
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          setShowText(!showText);
-        }}
-      >
-        Show Text
-      </button>
-
-      {showText && <Text />}
+      <h1>Generate an Excuse</h1>
+      <button onClick={ () => generateExcuse("party") }>Party</button>
+      <button onClick={ () => generateExcuse("family") }>Family</button>
+      <button onClick={ () => generateExcuse("office") }>Office</button>
+      <p>{excuse}</p>
     </div>
   );
 }
