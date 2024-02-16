33d312c8a40c599db670672528d5c3e15b0a1ad4
Dan PN <77532827+Derlvx@users.noreply.github.com>
2024-02-16 17:40:24 -0500

    React ep8

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
-import Axios from "axios";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Home } from "./pages/home";
+import { About } from "./pages/about";
+import { Contact } from "./pages/contact";
+import { Navbar } from "./pages/navbar";
 
 
 function App() {
 
-  const [excuse, setExcuse] = useState("");
-
-  const generateExcuse = (category) => {
-    let url = "https://excuser-three.vercel.app/v1/excuse/" + category;
-    Axios.get(url).then((response) => {
-      setExcuse(response.data[0].excuse);
-    })
-  }
-
   return (
     <div className="App">
-      <h1>Generate an Excuse</h1>
-      <button onClick={ () => generateExcuse("party") }>Party</button>
-      <button onClick={ () => generateExcuse("family") }>Family</button>
-      <button onClick={ () => generateExcuse("office") }>Office</button>
-      <p>{excuse}</p>
+
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/about" element={ <About /> } />
+          <Route path="/contact" element={ <Contact /> } />
+          <Route path="*" element={ <h1>Not Found</h1> } />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
