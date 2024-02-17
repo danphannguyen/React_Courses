825080ba33d226d3e6685fef332b54d4f7484e88
Dan PN <77532827+Derlvx@users.noreply.github.com>
2024-02-17 17:06:24 -0500

    React ep9

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Home } from "./pages/home";
-import { About } from "./pages/about";
+import { Profile } from "./pages/profile";
 import { Contact } from "./pages/contact";
 import { Navbar } from "./pages/navbar";
+import { useState, createContext } from "react";
 
+export const AppContext = createContext();
 
 function App() {
+
+  const [username, setUsername] = useState("Derlax");
 
   return (
     <div className="App">
 
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={ <Home /> } />
-          <Route path="/about" element={ <About /> } />
-          <Route path="/contact" element={ <Contact /> } />
-          <Route path="*" element={ <h1>Not Found</h1> } />
-        </Routes>
-      </Router>
+      <AppContext.Provider value={{username, setUsername}}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<h1>Not Found</h1>} />
+          </Routes>
+        </Router>
+      </AppContext.Provider>
 
     </div>
   );
--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -8,7 +8,7 @@ export const Navbar = () => {
                     <Link to="/">Home</Link>
                 </li>
                 <li>
-                    <Link to="/about">About</Link>
+                    <Link to="/profile">Profile</Link>
                 </li>
                 <li>
                     <Link to="/contact">Contact</Link>
